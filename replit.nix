dc7f07ac1ca00f80c2bb410f599adf98ba66e5eb
mocchan <tomohiko0629@gmail.com>
2025-03-28 14:34:59 +0900

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -4,7 +4,11 @@
     pkgs.python311Packages.pip
     pkgs.python311Packages.flask
     pkgs.python311Packages.python-dotenv
-    pkgs.python311Packages.openai
-    pkgs.wget
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wget
+  ];
+
+  pip = [
+    "openai==0.27.10"
   ];
 }
